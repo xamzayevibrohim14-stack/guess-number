81d98ff66c5a4e120f7365ac4e4ad7b8e6508af7
ibrohim-ultras24 <xamzayevibrohim14@gmail.com>
2025-10-22 14:12:47 +0500

    add chance

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import random
 
 secret_number = random.randint(1, 10)
 
-while True:
+count = 0
+while count < 3:
     guess = int(input("enter guess number: "))
     if guess == secret_number:
         print("you win ✅")
         break
     else:
-        print("you lose ❌")
+        print(f"you lose ❌>> {count}")
+    count += 1
